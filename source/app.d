6164ecdf3197243efd52e69476a32b11bb1b52de
Nafees <nafeeshassan16@gmail.com>
2017-12-21 03:43:19 +0500

    Finalized all features & commands.
    Signed-off-by: Nafees <nafeeshassan16@gmail.com>

--- source/app.d
+++ source/app.d
@@ -1,7 +1,11 @@
+import std.datetime;
+import std.process;
 import std.stdio;
 import std.path;
 import std.file;
 import bak;
+
+import utils.misc;
 
 const VERSION = "0.1.0";
 const CONF_PATH = "~/.config/simplebak/conf.sdl";
@@ -36,24 +40,30 @@ Run without any command to start make backup of all added files/dirs\n"
 			ConfigFile conf;
 			conf.openConfig(expandTilde(CONF_PATH));
 			if (args[1] == "add"){
-				// check if file exists
 				// check if is valid path
 				if (args.length < 3){
 					writeln ("No file/dir specified to add to backups");
+				}else if (!args[2].isValidFilename){
+					writeln ("Invalid filename specified");
+				}else if (!args[2].exists){
+					writeln ("Filename specified does not exist");
 				}else{
-					if (!args[2].isValidFilename){
-						writeln ("Invalid filename specified");
-					}else if (!args[2].exists){
-						writeln ("Filename specified does not exist");
-					}else{
-						// now just add it
-						conf.filePaths = conf.filePaths ~ args[2];
-						writeln (baseName(args[2])~" added");
-					}
+					// now just add it
+					conf.filePaths = conf.filePaths ~ absolutePath(args[2]);
+					writeln (baseName(args[2])~" added");
 				}
 			}else if (args[1] == "remove"){
-				// check if it exists in the tobak.sdl
-				// TODO implement remove command
+				// check if specified
+				if (args.length < 3){
+					writeln ("No file/dir specifed to remove from future backups");
+				}else if (!conf.filePaths.indexOf(absolutePath(args[2]))){
+					writeln (baseName(args[2])~" was never added, cannot be removed");
+				}else{
+					// remove it
+					uinteger index = conf.filePaths.indexOf(args[2].absolutePath);
+					conf.filePaths = conf.filePaths.deleteElement(index);
+					writeln (baseName(args[2])~" removed from future backups");
+				}
 			}else{
 				writeln (args[1], "is not a valid command.\nType 'simplebak help' for list of available commands");
 			}
@@ -61,6 +71,29 @@ Run without any command to start make backup of all added files/dirs\n"
 			conf.saveConfig();
 		}
 	}else{
-		// TODO make backup
+		// load the conf file
+		ConfigFile conf;
+		conf.openConfig(expandTilde(CONF_PATH));
+		// execute start command
+		if (conf.backupStartCommand != ""){
+			executeShell(conf.backupStartCommand);
+		}
+		// check if any files have been modified, then make backup
+		foreach (file; conf.filePaths){
+			// get backup date
+			SysTime backupDate = BakMan.lastBackupDate(file);
+			// now see if modified, then back it up
+			if (BakMan.hasModified(file, backupDate)){
+				if (BakMan.makeBackup(file)){
+					if (conf.backupFailCommand != ""){
+						executeShell(conf.backupFailCommand);
+					}
+				}
+			}
+		}
+		// execute backup-end-command
+		if (conf.backupFinishCommand != ""){
+			executeShell(conf.backupFinishCommand);
+		}
 	}
 }
