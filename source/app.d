876f5f05356a611524a3bd3a5543e42d64dc9ebd
Nafees <nafeeshassan16@gmail.com>
2018-06-02 00:11:24 +0500

    Added exclude command
    Signed-off-by: Nafees <nafeeshassan16@gmail.com>

--- source/app.d
+++ source/app.d
@@ -4,18 +4,20 @@ import std.process;
 import std.stdio;
 import std.path;
 import std.file;
+import conf;
 import bak;
 
 import utils.misc;
 
-const VERSION = "0.1.0";
-const CONF_PATH = "~/.config/simplebak/conf.sdl";
+const VERSION = "1.0.0"; /// version
+const CONF_PATH = "~/.config/simplebak/conf.sdl"; /// file to store config
 
 /*
 Args format:
 	simplebak [command]
 Commands:
 	add 	- adds a new file/dir for backup.
+	exclude - excludes a file/dir from being included in any backup.
 	remove 	- removes a file/dir from list of files to make backup of.
 	backup	- makes backup of only a specified file, if needed
 	help	- displays this message
@@ -31,15 +33,14 @@ void main(string[] args){
 \tsimplebak [command]
 Commands:
 \tadd     - adds new files/dirs for making backups in future
+\texclude - excludes a file/dir from being included in any backup
 \tremove  - removes files/dirs from list of files to make backup of
 \tbackup  - makes backup of all added files/dirs
 \tlist    - displays list of all files/dirs to include in future backups
 \thelp    - displays this message
-\tversion - displays the version\n
-Run without any command to start make backup of all added files/dirs\n"
+\tversion - displays the version\n"
 		);
-	}
-	if (args.length > 1){
+	}else if (args.length > 1){
 		if (args[1] == "version"){
 			writeln (VERSION);
 		}else{
@@ -51,22 +52,20 @@ Run without any command to start make backup of all added files/dirs\n"
 				if (args.length < 3){
 					writeln ("No file/dir specified to add to backups");
 					exitCode = 1;
-				}else if (!args[2].exists){
-					// check if all files actually exist, add those that exist, ignore those that don't
-					/// stores how many files were not added
-					uinteger notAddedCount = 0;
+				}else{
+					// even if files/dirs don't exist, they will be added, coz they might exist in the future...
 					foreach (filePath; args[2 .. args.length]){
-						if (filePath.exists){
-							conf.filePaths = conf.filePaths ~ absolutePath(filePath);
-							writeln (filePath~" added");
-						}else{
-							notAddedCount ++;
-							writeln (filePath~" does not exist, not added");
-						}
+						conf.filePaths = conf.filePaths ~ absolutePath(filePath);
 					}
-					if (notAddedCount > 0){
-						writeln (notAddedCount," files/dirs were not added");
-						exitCode = 1;
+					writeln (args.length - 2," files/dirs added");
+				}
+			}else if (args[1] == "exclude"){
+				if (args.length < 3){
+					writeln ("No file/dir specified to be excluded from backups");
+					exitCode = 1;
+				}else{
+					foreach (filePath; args[2 .. args.length]){
+						conf.excludeList = conf.excludeList ~ absolutePath(filePath);
 					}
 				}
 			}else if (args[1] == "remove"){
@@ -75,62 +74,23 @@ Run without any command to start make backup of all added files/dirs\n"
 					writeln ("No file/dir specifed to remove from future backups");
 					exitCode = 1;
 				}else{
-					// check if each file was added before, remove those that were added
-					/// stores how many files/dirs were not removed
-					uinteger notRemovedCount = 0;
+					// check if each file was added before, remove those that were added, ignore the rest
+					uinteger count = 0;
 					foreach (filePath; args[2 .. args.length]){
-						integer index =conf.filePaths.indexOf(absolutePath(filePath));
+						integer index = conf.filePaths.indexOf(absolutePath(filePath));
 						if (index >= 0){
 							conf.filePaths = conf.filePaths.deleteElement(index);
-							writeln (filePath~" remove from future backups");
-						}else{
-							notRemovedCount ++;
-							writeln (filePath~" was never added, cannot be removed");
+							count ++;
 						}
 					}
-					if (notRemovedCount > 0){
-						writeln (notRemovedCount," files/dirs were not removed");
-						exitCode = 1;
-					}
+					writeln (count, "files/dirs removed from future backups");
 				}
 			}else if (args[1] == "list"){
 				foreach (filePath; conf.filePaths){
 					writeln (filePath);
 				}
 			}else if (args[1] == "backup"){
-				writeln ("Total Files to make backup for: ", conf.filePaths.length);
-				// execute start command
-				if (conf.backupStartCommand != ""){
-					writeln ("Executing pre-makeBackup shell command:");
-					writeln ("Command returned: ",executeShell(conf.backupStartCommand).output);
-				}
-				// check if any files have been modified, then make backup
-				foreach (file; conf.filePaths){
-					// get backup date
-					SysTime backupDate = BakMan.lastBackupDate(file);
-					// now see if modified, then back it up
-					if (BakMan.hasModified(file, backupDate)){
-						writeln (file, " has been modified, making new backup");
-						if (!BakMan.makeBackup(file)){
-							writeln ("Backup failed");
-							exitCode = 1;
-							if (conf.backupFailCommand != ""){
-								writeln ("Executing backup-fail shell command:");
-								writeln ("Command returned: ", executeShell(conf.backupFailCommand).output);
-							}
-						}else{
-							writeln ("Backup successful");
-						}
-					}else{
-						writeln (file, " not modified since last backup, skipping");
-					}
-				}
-				writeln ("Backup finished");
-				// execute backup-end-command
-				if (conf.backupFinishCommand != ""){
-					writeln ("Executing backup-finish shell command:");
-					writeln ("Command returned: ", executeShell(conf.backupFinishCommand).output);
-				}
+				// TODO
 			}else{
 				writeln (args[1], "is not a valid command.\nType 'simplebak help' for list of available commands");
 			}
